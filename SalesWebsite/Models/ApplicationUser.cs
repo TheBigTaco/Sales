2aa9b68514a9dac41b35f223274a2b6925355ce7
Adam Titus and James Osborn <adam+james@>
2018-01-17 10:37:10 -0800

    Basic user authentication.

---
+++ SalesWebsite/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+namespace SalesWebsite.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+        }
+    }
+}
---
+++ SalesWebsite/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesWebsite.Models
+{
+    [Table("products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
